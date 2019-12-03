262bdeec2de0d83b6ff70015432a569b2e19b62d
andres7xd <58347580+andres7xd@users.noreply.github.com>
2019-12-03 08:06:35 -0600

    Paso dos ( Dibujar el dado)

--- Dados/Program.cs
+++ Dados/Program.cs
@@ -8,10 +8,33 @@ namespace Dados
         {   while(Console.ReadKey().Key == ConsoleKey.Enter)
             {
                 Console.WriteLine("Nueva visualizacion");
+                DibujarDado(4, 5, 1);
+                
             }
 
+           
+            
             
         
         }
+
+        static void DibujarDado(int caraSuperior, int caraFrontal, int caraIsquierda)
+        {
+
+            Console.WriteLine("                     ###########            ");
+            Console.WriteLine("                     #         #");
+            Console.WriteLine("                     #    {0}    #", caraSuperior);
+            Console.WriteLine("                     #         #");
+            Console.WriteLine("#########################################");
+            Console.WriteLine("#         #          #          #       #");
+            Console.WriteLine("#   {1}     #     {2}    #    {0}     #   {3}   #", caraFrontal, Math.Abs(caraFrontal - 7), caraIsquierda, Math.Abs(caraIsquierda - 7));
+            Console.WriteLine("#         #          #          #       #");
+            Console.WriteLine("#########################################");
+            Console.WriteLine("                     #         #");
+            Console.WriteLine("                     #    {0}    #", Math.Abs(caraSuperior - 7));
+            Console.WriteLine("                     #         #");
+            Console.WriteLine("                     ###########            ");
+        }
+
     }
 }
