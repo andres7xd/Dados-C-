69e6b3643105d81aaec4a95b7a7657deffa032d2
andres7xd <58347580+andres7xd@users.noreply.github.com>
2019-12-02 19:14:17 -0600

    primer paso (imprime nueva vizualizacion)

---
+++ Dados/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   while(Console.ReadKey().Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine("Nueva visualizacion");
+            }
+
+            
+        
+        }
+    }
+}
